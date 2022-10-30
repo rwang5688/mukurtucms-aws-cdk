c3deda2c5dfb2642d175afa376f75d2b3fff890b
Robert Wang <wangrob@amazon.com>
2022-10-30 01:07:04 -0700

    create mukurtucms aws cdk stack baseline

--- lib/mukurtucms-aws-cdk-stack.ts
+++ lib/mukurtucms-aws-cdk-stack.ts
@@ -1,19 +1,138 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import * as iam from "aws-cdk-lib/aws-iam";
+import {readFileSync} from 'fs';
 import { Construct } from 'constructs';
 
-export class MukurtucmsAwsCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class MukurtucmsAwsCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    // Parameter: Key Pair
+    const ec2KeyPairParam = new cdk.CfnParameter(this, "ec2KeyPair", {
+      type: "String",
+      default: "ec2-key-pair",
+      description: "Key pair for the EC2 instance.",
+    });
+    const ec2KeyPair = ec2KeyPairParam.valueAsString;
 
-    const queue = new sqs.Queue(this, 'MukurtucmsAwsCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // Create VPC
+    const vpc = new ec2.Vpc(this, "mukurtucms-vpc", {
+      cidr: "10.0.0.0/16",
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: "mukurtucms-public-",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+          mapPublicIpOnLaunch: false,
+        },
+      ],
     });
 
-    const topic = new sns.Topic(this, 'MukurtucmsAwsCdkTopic');
+    // Create RDS Secret
+    const rdsSecret = new secretsmanager.Secret(this, "mukurtucms-rds-secret", {
+      secretName: "mukurtucms-rds-secret",
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: "admin" }),
+        generateStringKey: "password",
+        excludePunctuation: true,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // Create RDS Instance Security Group
+    const rdsInstanceSG = new ec2.SecurityGroup(this, "mukurtucms-rds-sg", {
+      vpc: vpc,
+      allowAllOutbound: true,
+      description: "Mukurtu CMS RDS Instance security group",
+    });
+
+    // Create RDS Instance (m6i.large, 2 vPUCs, 8 GiB RAM, io1, 100 GiB, 1500 IOPS)
+    const rdsInstance = new rds.DatabaseInstance(this, "mukurtucms-rds-instance", {
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_0_30,
+      }),
+      vpc: vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      credentials: rds.Credentials.fromSecret(rdsSecret),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      securityGroups: [rdsInstanceSG],
+      storageEncrypted: true,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.M6I,
+        ec2.InstanceSize.LARGE
+      ),
+      publiclyAccessible: false,
+      allocatedStorage: 100,
+      storageType: rds.StorageType.IO1,
+      iops: 1500,
+      instanceIdentifier: "mukurtucms-rds-instance",
+    });
+
+    // Create EC2 Instance Security Group
+    const ec2InstanceSG = new ec2.SecurityGroup(this, "mukurtucms-ec2-sg", {
+      vpc: vpc,
+      allowAllOutbound: true,
+      description: "Mukurtu CMS EC2 Instance security group",
+    });
+
+    ec2InstanceSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+    ec2InstanceSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    ec2InstanceSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+    // Allow EC2 Instance SG to communicate to RDS Instance SG
+    rdsInstanceSG.connections.allowFrom(
+      new ec2.Connections({ securityGroups: [ec2InstanceSG] }),
+      ec2.Port.tcp(3306),
+      "EC2 Instance to RDS Instance (MySQL)"
+    );
+
+    // Create a Role for the EC2 Instance (w/ AmazonS3ReadOnlyAccess as sample policy)
+    const ec2InstanceRole = new iam.Role(this, "mukurtucms-ec2-role", {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),
+      ],
+    });
+
+    // Create the EC2 Instance (m6i.large, 2 vCPUs, 8 GiB RAM, )
+    const ec2Instance = new ec2.Instance(this, "mukurtucms-ec2-instance", {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      role: ec2InstanceRole,
+      securityGroup: ec2InstanceSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.M6I,
+        ec2.InstanceSize.LARGE,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      keyName: ec2KeyPair,
+    });
+
+    // Load contents of script
+    const userDataScript = readFileSync('./lib/user-data.sh', 'utf8');
+    // Add the User Data script to the Instance
+    ec2Instance.addUserData(userDataScript);
   }
 }
---
+++ lib/user-data.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+sudo yum update -y
+
+# install PHP and required extensions
+sudo amazon-linux-extras install -y epel
+sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --disable remi-php54
+yum-config-manager --enable remi-php74
+
+sudo amazon-linux-extras install -y php7.4
+sudo yum install -y php-{gd,mbstring}
+
+# confirm that we have installed PHP 7.4.
+php --version >> install.out
+
+# sudo nano /etc/php.ini and set the following parameters
+# reboot to make sure these take effect
+# Resource limits
+#max_execution_time = 240
+#memory_limit = 512M
+# Data handling
+#post_max_size = 256M
+# File upload
+#upload_max_filesize = 256M
+
+# confirm initial values of the following parameters that we have to change
+grep max_execution_time /etc/php.ini >> install.out
+grep memory_limit /etc/php.ini >> install.out
+grep post_max_size /etc/php.ini >> install.out
+grep upload_max_filezie /etc/php.ini >> install.out
+
+# install ImageMagick
+sudo yum install -y php-pear php-devel gcc
+sudo yum install -y ImageMagick ImageMagick-devel ImageMagick-perl
+
+# confirm that we have installed ImageMagick
+convert --version >> install.out
+gm version >> install.out
+
+# install Apache web server
+sudo yum -y install httpd
+sudo systemctl enable --now httpd
+
+sudo yum install -y firewalld
+sudo systemctl enable --now firewalld
+
+sudo firewall-cmd --add-service={http,https} --permanent
+sudo firewall-cmd --reload
+
+# confirm that we have installed and started Apache web server
+systemctl status httpd >> install.out
+
+# create vhost with Apache access and error logs
+cd /etc/httpd/conf.d
+sudo cat << EOF > vhosts.conf
+<VirtualHost *:80>
+  # The name your website should respond to
+  ServerName mukurtu.rwang5688.com
+
+  # if you want this vhost to listen to extra names, uncomment the next line
+  # ServerAlias foo.com www.bar.com bar.com
+
+  # Tell Apache where your document root is
+  DocumentRoot /var/www/html/mukurtucms
+
+  # Add this line if you are allowing .htaccess overrides.
+  <Directory /var/www/html/mukurtucms>
+    AllowOverride All
+  </Directory>
+
+  CustomLog /var/log/apache/mukurtu.rwang5688.com-access.log combined
+  ErrorLog /var/log/apache/mukurtu.rwang5688.com-error.log
+</VirtualHost>
+EOF
+sudo mkdir /var/log/apache
+sudo service httpd restart
+
+# confirm that we have correctly created vhost
+cat /etc/httpd/conf.d/vhosts.conf >> install.out
+
+# install git and download mukurtucms
+sudo yum install -y git
+cd
+mkdir workspace
+cd workspace
+git clone https://github.com/MukurtuCMS/mukurtucms.git
+cp mukurtucms/sites/default/default.settings.php settings.php
+
+# confirm that we have successfully copied settings.php
+ls -l mukurtucms/sites/default/settings.php >> install.out
+
+# install mysql client
+sudo yum install -y mysql
+
+# confirm that we have installed MySQL client
+mysql --version >> install.out
+
+# one-time setup setps prior to copy to /var/www/html
+# under SecretsManager > projectName-rds-secret, look up the admin password
+# under RDS > Databases > projectName-rds-instance, look up the hostname
+# nano mukurtucms/sites/default/settings.php (the copy)
+# fill in: database name = mukurtucms, username = admin, password = admin_password, host = hostname
+
+# recurring setup steps for updating /var/www/html
+# mysql -h hostname -P 3306 -u admin -p admin_password
+# <MYSQL> drop database mukurtucms
+# <MYSQL> create database mukurtucms;
+# cd ~/workspace
+# sudo rm -rf /var/www/html/mukurtucms
+# sudo cp -r mukurtucms /var/www/html/
+# sudo chown -R apache:apache /var/www/html/mukurtucms
+# sudo service httpd restart
