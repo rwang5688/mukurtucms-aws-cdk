8cfd5e34f430ff9a0cb4f749063a07e1a71df43a
Robert Wang <wangrob@amazon.com>
2022-10-30 18:27:23 -0700

    use linux script session to record installation

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# use linux script session to record the installation
+script /home/ec2-user/install.out
+
+# apply latest patches
 sudo yum update -y
 
 # install PHP and required extensions
@@ -13,31 +17,20 @@ sudo amazon-linux-extras install -y php7.4
 sudo yum install -y php-{gd,mbstring}
 
 # confirm that we have installed PHP 7.4.
-php --version >> /home/ec2-user/install.out
-
-# sudo nano /etc/php.ini and set the following parameters
-# reboot to make sure these take effect
-# Resource limits
-#max_execution_time = 240
-#memory_limit = 512M
-# Data handling
-#post_max_size = 256M
-# File upload
-#upload_max_filesize = 256M
+php --version
 
 # confirm initial values of the following parameters that we have to change
-grep max_execution_time /etc/php.ini >> /home/ec2-user/install.out
-grep memory_limit /etc/php.ini >> /home/ec2-user/install.out
-grep post_max_size /etc/php.ini >> /home/ec2-user/install.out
-grep upload_max_filezie /etc/php.ini >> /home/ec2-user/install.out
+grep max_execution_time /etc/php.ini
+grep memory_limit /etc/php.ini
+grep post_max_size /etc/php.ini
+grep upload_max_filezie /etc/php.ini
 
 # install ImageMagick
 sudo yum install -y php-pear php-devel gcc
 sudo yum install -y ImageMagick ImageMagick-devel ImageMagick-perl
 
 # confirm that we have installed ImageMagick
-convert --version >> /home/ec2-user/install.out
-#gm version >> /home/ec2-user/install.out
+convert --version
 
 # install Apache web server
 sudo yum -y install httpd
@@ -50,7 +43,7 @@ sudo firewall-cmd --add-service={http,https} --permanent
 sudo firewall-cmd --reload
 
 # confirm that we have installed and started Apache web server
-systemctl status httpd >> /home/ec2-user/install.out
+systemctl status httpd
 
 # create vhost with Apache access and error logs
 cd /etc/httpd/conf.d
@@ -78,7 +71,7 @@ sudo mkdir /var/log/apache
 sudo service httpd restart
 
 # confirm that we have correctly created vhost
-cat /etc/httpd/conf.d/vhosts.conf >> /home/ec2-user/install.out
+cat /etc/httpd/conf.d/vhosts.conf
 
 # install git and download mukurtucms
 sudo yum install -y git
@@ -90,24 +83,47 @@ cp mukurtucms/sites/default/default.settings.php settings.php
 sudo chown -R ec2-user:ec2-user /home/ec2-user/workspace
 
 # confirm that we have successfully copied settings.php
-ls -l mukurtucms/sites/default/settings.php >> /home/ec2-user/install.out
+ls -l mukurtucms/sites/default/settings.php
 
 # install mysql client
 sudo yum install -y mysql
 
 # confirm that we have installed MySQL client
-mysql --version >> /home/ec2-user/install.out
+mysql --version
 
-# one-time setup setps prior to copy to /var/www/html
+# finish and flush the linux script session
+exit
+
+## one-time setup setps prior to copy to /var/www/html
+
+# sudo nano /etc/php.ini and set the following parameters:
+# Resource limits
+#max_execution_time = 240
+#memory_limit = 512M
+# Data handling
+#post_max_size = 256M
+# File upload
+#upload_max_filesize = 256M
+
+# reboot to make sure the /etc/php.ini parameter changes take effect
+
 # under SecretsManager > projectName-rds-secret, look up the admin password
 # under RDS > Databases > projectName-rds-instance, look up the hostname
-# nano mukurtucms/sites/default/settings.php (the copy)
-# fill in: database name = mukurtucms, username = admin, password = admin_password, host = hostname
+# nano mukurtucms/sites/default/settings.php (the copy) and set the following parameters:
+# Database parameters
+#name = mukurtucms
+#username = admin
+#password = admin_password
+#host = hostname
 
-# recurring setup steps for updating /var/www/html
+## recurring setup steps for updating /var/www/html
+
 # mysql -h hostname -P 3306 -u admin -p admin_password
-# <MYSQL> drop database mukurtucms
+# <MYSQL> show databases;
+# <MYSQL> drop database mukurtucms;
 # <MYSQL> create database mukurtucms;
+# <MYSQL> exit;
+
 # cd ~/workspace
 # sudo rm -rf /var/www/html/mukurtucms
 # sudo cp -r mukurtucms /var/www/html/
